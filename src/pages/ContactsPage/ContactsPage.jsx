c62b36e575d7e97849c8e8db11321caefe553e7d
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-05-12 16:36:01 +0300

    fix contactsPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,18 @@
 import ContactList from '../../components/ContactList/ContactList';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import SearchBox from '../../components/SearchBox/SearchBox';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react'; 
+import { fetchContacts } from '../../redux/contacts/operations';
 
 const ContactsPage = () => {
+
+   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Contacts Page</h2>
